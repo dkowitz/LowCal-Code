007a3bac2cbecd7be0073331093e21e180391abc
dkowitz <dkowitz@gmail.com>
2025-09-28 19:06:13 +0000

    Ignore generated build files

--- packages/core/dist/src/generated/git-commit.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @license
- * Copyright 2025 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-export declare const GIT_COMMIT_INFO = "430e0be";
-export declare const CLI_VERSION = "0.0.13";
--- packages/core/dist/src/ide/constants.js
+++
@@ -1,7 +0,0 @@
-/**
- * @license
- * Copyright 2025 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-export const QWEN_CODE_COMPANION_EXTENSION_NAME = 'Qwen Code Companion';
-//# sourceMappingURL=constants.js.map
