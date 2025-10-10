e009717ff7506afa820792587861fcab9ac07c62
dkowitz <dkowitz@gmail.com>
2025-10-10 19:49:11 +0000

    Reverting to good /view command and trying to merge to main

--- packages/cli/dist/src/generated/git-commit.d.ts
+++ packages/cli/dist/src/generated/git-commit.d.ts
@@ -3,5 +3,5 @@
  * Copyright 2025 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
-export declare const GIT_COMMIT_INFO = "012fd50";
+export declare const GIT_COMMIT_INFO = "fd2147c7";
 export declare const CLI_VERSION = "0.0.13";
