f10288a06e01326e5460f7bd39956db46bd6f211
dkowitz <dkowitz@gmail.com>
2025-10-19 12:45:07 +0000

    Added linux image pasting support, but not currently working on Ubuntu 24.04 Gnome

--- packages/cli/dist/src/generated/git-commit.d.ts
+++ packages/cli/dist/src/generated/git-commit.d.ts
@@ -3,5 +3,5 @@
  * Copyright 2025 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
-export declare const GIT_COMMIT_INFO = "0022b187";
+export declare const GIT_COMMIT_INFO = "f2a618be";
 export declare const CLI_VERSION = "0.0.13";
